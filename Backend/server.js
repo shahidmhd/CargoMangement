1638228d644fdca128a1894ce0c28d719ccaad58
shahidmhd <shahidvk124@gmail.com>
2023-08-11 09:42:06 +0530

    cors changed

--- Backend/server.js
+++ Backend/server.js
@@ -14,22 +14,27 @@ dotenv.config()
 
 const port = process.env.PORT;
 app.use(express.json());
-app.use(cors())
+const corsOptions = {
+  origin: ['https://cargo-management.onrender.com', 'https://www.cargo-management.onrender.com'],
+  methods: ['GET', 'POST'],
+};
+
+app.use(cors(corsOptions));
 app.use(morgan('dev'))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 
-app.use('/api/users',UserRouters)
+app.use('/api/users', UserRouters)
 app.use(errorHandlingMidlleware)
 
 // console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log("\x1b[33m%s\x1b[0m", "Database connected successfully"); // Yellow color code: \x1b[33m
-  }).catch((error) => {
-    console.log("\x1b[31m%s\x1b[0m", `Database connection error: ${error}`); // Red color code: \x1b[31m
-  });
-  
+  console.log("\x1b[33m%s\x1b[0m", "Database connected successfully"); // Yellow color code: \x1b[33m
+}).catch((error) => {
+  console.log("\x1b[31m%s\x1b[0m", `Database connection error: ${error}`); // Red color code: \x1b[31m
+});
+
 app.listen(port, () => {
   console.log("\x1b[34m%s\x1b[0m", `Server listening at port ${port}`);
 });
